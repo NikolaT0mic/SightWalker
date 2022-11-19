9773175441e72528b1530c491d966de3981768b5
Nikola <niktom@web.de>
2022-11-19 10:54:11 +0100

    cleaned up comments, added google_maps_package

---
+++ sight_walker/lib/views/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.amber),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            alignment: Alignment.centerLeft,
+            height: 40,
+            width: double.infinity,
+            margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(5.0),
+            child: const Text("Search"),
+          ),
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
